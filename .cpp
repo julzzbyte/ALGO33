4b0b59f74bc8fdda62b9e48684256cd625fe43a3
Lawson <amazingeyins@gmail.com>
2022-02-07 13:35:27 +0100

    second commit

---
+++ .cpp
@@ -0,0 +1,42 @@
+CTrade trade;
+datetime TimeCurrent();     
+int WaitTime = 05;  // 05 Min.
+static int TimeSent;
+//int curr_positions=PositionsTotal();
+//int OrdersTotal();
+
+void OnTick()
+  {
+     double Ask=NormalizeDouble(SymbolInfoDouble(_Symbol,SYMBOL_ASK),_Digits);
+     double Balance=AccountInfoDouble(ACCOUNT_BALANCE);
+     double Equity=AccountInfoDouble(ACCOUNT_EQUITY);
+     double Bid=NormalizeDouble(SymbolInfoDouble(_Symbol, SYMBOL_BID),_Digits);
+     
+     if(PositionsTotal()==0)
+     trade.Buy(
+     0.01,
+     NULL,
+     Ask,
+     0,(Ask+100 * _Point),
+     NULL
+     );
+     
+     
+
+      if (TimeLocal() >= TimeSent + (WaitTime * 60) && Equity<Balance)
+         {
+              trade.Sell(
+              0.02,
+              NULL,
+              Ask,
+              0,(Bid-100 * _Point),
+              NULL
+              );
+     
+         }
+     
+     
+   
+     
+   
+  }
